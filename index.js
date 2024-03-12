9b63c5c47eed74f32afec93f6912e79e4a4fd951
Ibrahimkhan <ibrahimsheikh772@gmail.com>
2024-03-13 00:48:58 +0500

    practice

---
+++ index.js
@@ -0,0 +1,47 @@
+console.log("hi");
+//     *dicevale
+// var a = Math.random();
+// var dicevalue = Math.ceil(a * 6);
+// console.log(dicevalue);
+
+//     *coinvalue
+// var a = Math.random();
+// var coinvalue = Math.floor(a * 2);
+// console.log(coinvalue); 
+
+// console.log(typeof NaN)
+
+// if ( "NaN"){
+//     console.log("true")
+// }
+// else {
+// console.log("false")
+// }
+
+    //    * convert string to number
+    // var a = "10.5"
+    //  var b = +a
+    // var b =Number(a)
+    // var b =parseFloat(a)
+    // var b =parseInt(a)
+    // var b =a.toString(); 
+    
+    //  *seperate like 1,234,567,
+//     var a = 1.059328438 
+//     var b =a.toLocaleString();    
+// console.log(b)
+// var brandList
+
+    // *assign loop answer =30
+// var a = 0;
+// for (var i=0; i<10; i++){
+// a +=  3;
+// }
+// console.log(a)
+
+
+    //  *Cunstructor function of date and time  
+// var currentDate = new Date( );
+// console.log(currentDate);
+// console.log(currentDate.getDate());
+// console.log(currentDate.getMonth());
